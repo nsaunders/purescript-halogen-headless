615d7d9536a135b64f789105f78bc0a97457f168
Nick Saunders <nick@saunde.rs>
2022-01-29 09:29:36 -0700

    Formatting

--- stories/Index.purs
+++ stories/Index.purs
@@ -7,4 +7,4 @@ import Halogen.Hooks as Hooks
 
 component :: forall q i o m. Component q i o m
 component = Hooks.component \_ _ -> Hooks.do
-  Hooks.pure $ HH.div_ [HH.text "TODO"]
+  Hooks.pure $ HH.div_ [ HH.text "TODO" ]
