f52bee285b6a3b01506eb2c23f883d88c1ba7460
Nick Saunders <nick@saunde.rs>
2022-01-28 17:32:44 -0700

    Storybook progress

--- stories/Main.purs
+++ stories/Main.purs
@@ -2,19 +2,21 @@ module Main where
 
 import Prelude
 
-import Data.Maybe (Maybe(Nothing))
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Class (class MonadEffect)
 import Foreign.Object (fromFoldable) as Object
 import Halogen.Aff (awaitBody, runHalogenAff)
+import Halogen.HTML as HH
+import Halogen.Hooks as Hooks
 import Halogen.Storybook (Stories, runStorybook, proxy)
 import Stories.Accordion (component) as Accordion
 import Stories.AccordionItem (component) as AccordionItem
 
 stories :: forall m. MonadEffect m => Stories m
 stories = Object.fromFoldable
-  [ Tuple "" $ proxy Accordion.component
+  [ Tuple "" $ proxy $ Hooks.component \_ _ -> Hooks.pure $ HH.text "todo"
+  , Tuple "Accordion" $ proxy Accordion.component
   , Tuple "AccordionItem" $ proxy AccordionItem.component
   ]
 
@@ -23,5 +25,5 @@ main = runHalogenAff $
   awaitBody >>=
     runStorybook
       { stories
-      , logo: Nothing
+      , logo: pure (HH.text "Halogen Headless")
       }
