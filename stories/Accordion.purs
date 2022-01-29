e8813f13de0b711bf9c873337c61ac36e0d8ac23
Nick Saunders <nick@saunde.rs>
2022-01-29 11:11:12 -0700

    Accordion FAQs

--- stories/Accordion.purs
+++ stories/Accordion.purs
@@ -3,6 +3,7 @@ module Stories.Accordion where
 import Prelude
 
 import Data.Tuple (Tuple(..))
+import Data.Tuple.Nested ((/\))
 import Effect.Class (class MonadEffect)
 import Halogen (Component)
 import Halogen.HTML as HH
@@ -15,10 +16,27 @@ component :: forall q i o m. MonadEffect m => Component q i o m
 component =
   let
     items =
-      [ Tuple 1 $ Tuple [ HH.text "Summary 1" ] [ HH.text "Details 1" ]
-      , Tuple 2 $ Tuple [ HH.text "Summary 2" ] [ HH.text "Details 2" ]
-      , Tuple 3 $ Tuple [ HH.text "Summary 3" ] [ HH.text "Details 3" ]
+      [ 1 /\
+        "What is a headless component?" /\ """
+        A headless component addresses concerns like state, accessibility, and
+        keyboard support while allowing you to focus on implementing your own
+        visual design.
+        """
+      , 2 /\
+        "Is the Accordion component accessible?" /\ """
+        The Accordion component implements the WAI-ARIA Accordion Design Pattern
+        which provides an accessible markup structure to support screen reader
+        and keyboard users.
+        """
+      , 3 /\
+        "How can I customize the Accordion component?" /\ """
+        The Accordion component provides minimalistic default HTML. You can
+        provide your own functions to render the desired heading, trigger, and
+        panel markup.
+        """
       ]
+      <#>
+        \(Tuple v (Tuple q a)) -> v /\ [HH.text q] /\ [HH.text a]
   in
     Hooks.component \_ _ -> Hooks.do
       accordion <- useAccordion
