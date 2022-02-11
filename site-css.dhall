e9d5f7889d0ea1621c4cbc3c2e18504673b3c8ec
Nick Saunders <nick@saunde.rs>
2022-02-11 06:00:05 -0700

    Better dev server experience.

--- site-css.dhall
+++ site-css.dhall
@@ -2,5 +2,5 @@ let conf = ./site.dhall
 
 in conf // {
   sources = conf.sources # ["site/CSS.purs"],
-  dependencies = conf.dependencies # ["console", "either", "exceptions", "node-buffer", "node-fs"]
+  dependencies = conf.dependencies # ["console", "either", "exceptions", "node-buffer", "node-fs", "node-process"]
 }
--- site/CSS.purs
+++ site/CSS.purs
@@ -1,21 +1,27 @@
 module Site.CSS where
 
 import Prelude
+
 import CSS (render, renderedSheet)
+import Data.Array (elemIndex, (!!))
 import Data.Either (either)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (fromMaybe, maybe)
 import Effect (Effect)
 import Effect.Class.Console (log, warn)
 import Effect.Exception (throwException)
 import Node.Encoding (Encoding(..))
 import Node.FS.Async (writeTextFile)
+import Node.Process (argv)
 import Site.Page.Accordion as Accordion
 
 main :: Effect Unit
-main =
-  case renderedSheet $ render Accordion.css of
-    Just cssString ->
-      writeTextFile UTF8 "./public/app.css" cssString $
+main = do
+  args <- argv
+  let to = fromMaybe "./index.css" $ "--to" `elemIndex` args >>= \i -> args !! (i + 1)
+  maybe
+    (warn "Warning: No CSS output")
+    (\cssString ->
+      writeTextFile UTF8 to cssString $
         either throwException \_ -> log "Successfully wrote CSS output."
-    Nothing ->
-      warn "Warning: No CSS output."
+    )
+    $ renderedSheet $ render Accordion.css
